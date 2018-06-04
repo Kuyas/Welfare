baee836a99995bc17a93044c1c4576a3ffb6fb09
Omkar Kanade <omukanade@gmail.com>
2018-06-04 12:37:59 +0530

    UserDetailsActivity and activity_user_details.xml added.

---
+++ app/src/main/java/com/example/android/welfare/BankingDetailsFragment.java
@@ -0,0 +1,25 @@
+package com.example.android.welfare;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BankingDetailsFragment extends Fragment {
+
+    public BankingDetailsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.test_layout_4, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/example/android/welfare/TradingDetailsFragment.java
@@ -0,0 +1,26 @@
+package com.example.android.welfare;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TradingDetailsFragment extends Fragment {
+
+    public TradingDetailsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.test_layout_2, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+
+}
