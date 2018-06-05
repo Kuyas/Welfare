a9aed18c398c5f94c9913d33ce7dce2a95472e02
Omkar Kanade <omukanade@gmail.com>
2018-06-05 20:34:08 +0530

    Merge branch 'master' of https://github.com/Kuyas/Welfare created activity_other_details

--- app/src/main/java/com/example/android/welfare/OtherDetailsFragment.java
+++ app/src/main/java/com/example/android/welfare/OtherDetailsFragment.java
@@ -13,7 +13,7 @@ public class OtherDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.test_layout_3, container, false);
+        View view = inflater.inflate(R.layout.activity_other_details, container, false);
         return view;
     }
 
